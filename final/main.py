1cc3d303cc006a7610e53cf18c085cc424c09c49
Brendan McGuire <brendan@bren.app>
2023-03-15 13:36:08 -0400

    Add game over check and clear filled lines

--- final/main.py
+++ final/main.py
@@ -20,8 +20,8 @@ game_board = board.Board("board", {})
 
 
 piece_bag = bag.Bag()
-tiles, color = piece_bag.get_next()
-game_board.add_tetromino((1, 1), tiles, color)
+tiles, image = piece_bag.get_next()
+game_board.add_tetromino((1, 1), tiles, image)
 
 last_move = time.time()
 last_player_move = time.time()
@@ -56,13 +56,13 @@ while True:
     if keys[pygame.K_DOWN] and time.time() - last_player_move > 0.05:
         game_board.move_tetromino(active_tetromino, 0, 1)
         last_player_move = time.time()
-    elif keys[pygame.K_LEFT] and time.time() - last_player_move > 0.1:
+    elif keys[pygame.K_LEFT] and time.time() - last_player_move > 0.15:
         game_board.move_tetromino(active_tetromino, -1, 0)
         last_player_move = time.time()
-    elif keys[pygame.K_RIGHT] and time.time() - last_player_move > 0.1:
+    elif keys[pygame.K_RIGHT] and time.time() - last_player_move > 0.15:
         game_board.move_tetromino(active_tetromino, 1, 0)
         last_player_move = time.time()
-    elif keys[pygame.K_UP] and time.time() - last_player_move > 0.1:
+    elif keys[pygame.K_UP] and time.time() - last_player_move > 0.15:
         game_board.rotate_tetromino(active_tetromino, "cw")
 
     # move the active tetromino down
@@ -74,10 +74,19 @@ while True:
     # if the active tetromino can't move down, create a new one
     if not game_board.tetromino_is_active(active_tetromino) and time.time() - last_player_move > 0.5:
 
-        tiles, color = piece_bag.get_next()
-        game_board.add_tetromino((board.BOARD_WIDTH // 2, 1), tiles, color)
+        tiles, image = piece_bag.get_next()
+        game_board.add_tetromino((board.BOARD_WIDTH // 2, 1), tiles, image)
         active_tetromino += 1
         last_move = time.time()
+
+        # Check if game over
+        if not game_board.tetromino_is_active(active_tetromino):
+            print("Game Over")
+            pygame.quit()
+            sys.exit()
+
+        # Clear filled lines
+        game_board.clear_lines()
 
     surface.fill(SCREEN_COLOR)
     entity.stepAll(surface)
