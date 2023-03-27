69d318096211018c249344a978e690020d23e319
Brendan McGuire <brendan@bren.app>
2023-03-27 15:17:59 -0400

    Render next up piece

--- final/systems/render_board.py
+++ final/systems/render_board.py
@@ -1,6 +1,7 @@
 from entities.board import Board, BOARD_WIDTH, BOARD_HEIGHT
 import systems.system as system
 import entities.entity as entity
+import entities.tetromino as tetromino
 import pygame
 
 
@@ -14,9 +15,19 @@ BOARD_Y = CELL_HEIGHT
 BOARD_COLOR = (255, 255, 255)
 GRID_LINE_COLOR = (200, 200, 200)
 
-BOARD_RECT = pygame.Rect(BOARD_X, BOARD_Y, BOARD_WIDTH_PX, BOARD_HEIGHT_PX)
+BOARD_BG_RECT = pygame.Rect(BOARD_X, BOARD_Y, BOARD_WIDTH_PX, BOARD_HEIGHT_PX)
 BOARD_TILE_RECTS = [[pygame.Rect(BOARD_X + x * CELL_WIDTH, BOARD_Y + y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
                      for x in range(BOARD_WIDTH)] for y in range(BOARD_HEIGHT)]
+
+BOARD_PREVIEW_WIDTH = 6
+BOARD_PREVIEW_HEIGHT = 10
+
+BOARD_PREVIEW_RECT = pygame.Rect(
+    BOARD_X + BOARD_WIDTH_PX + CELL_WIDTH, BOARD_Y, CELL_WIDTH * BOARD_PREVIEW_WIDTH, CELL_HEIGHT * BOARD_PREVIEW_HEIGHT)
+
+BOARD_PREVIEW_TILE_RECTS = [[pygame.Rect(BOARD_X + BOARD_WIDTH_PX + CELL_WIDTH + x * CELL_WIDTH, BOARD_Y + y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
+                             for x in range(BOARD_PREVIEW_WIDTH)] for y in range(BOARD_PREVIEW_HEIGHT)]
+
 
 IMAGES = {
     "BLUE": pygame.image.load("assets/BlueBlock.png"),
@@ -41,7 +52,8 @@ class RenderTetrisBoard(system.System):
 
     def run(self, entities):
         # Draw the board
-        pygame.draw.rect(self.surface, BOARD_COLOR, BOARD_RECT)
+        pygame.draw.rect(self.surface, BOARD_COLOR, BOARD_BG_RECT)
+        pygame.draw.rect(self.surface, BOARD_COLOR, BOARD_PREVIEW_RECT)
 
         # Draw grid lines
         for x in range(BOARD_WIDTH):
@@ -51,6 +63,13 @@ class RenderTetrisBoard(system.System):
         for y in range(BOARD_HEIGHT):
             pygame.draw.line(self.surface, GRID_LINE_COLOR, (BOARD_X,
                              BOARD_Y + y * CELL_HEIGHT), (BOARD_X + BOARD_WIDTH_PX, BOARD_Y + y * CELL_HEIGHT))
+
+        # Draw Up Next Tetromino
+        (tiles, color) = self.board.pieces[self.board.bag[-1]]
+        for offset in tiles:
+            tile = (offset[0] + 1, offset[1] + 1)
+            pygame.Surface.blit(
+                self.surface, IMAGES[color], BOARD_PREVIEW_TILE_RECTS[tile[1]][tile[0]])
 
         # Draw the tetrominos
         for y in range(BOARD_HEIGHT):
